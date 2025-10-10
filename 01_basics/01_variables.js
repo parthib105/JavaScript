052658d70a43bf4ce11ffb1aaf765b40e962ea50
Parthib Ghosh <121354394+parthib105@users.noreply.github.com>
2025-10-10 20:04:51 +0000

    completed conversion operations

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,20 @@ const accountID = 144553
 let accountEmail = "parthibg0@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-console.log(accountID)
+// accountID = 234     // not allowed
+
+accountEmail = "pg@ghosh.com"
+accountPassword = "3425"
+accountCity = "Kolkata"
+
+// console.log(accountID)
+
+// other way to print
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+ * var -> scope issue, doesn't care about scopes, if changed, all values in and out of the scope changes
+ * let -> resolved the issue of var
+*/
