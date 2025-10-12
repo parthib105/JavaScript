4dbab6f924269e8b71e316723a994b3010ddbd52
Parthib Ghosh <121354394+parthib105@users.noreply.github.com>
2025-10-12 17:45:48 +0000

    done kodule 1

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// // Number __proto__ functions
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) 
+
+// const otherNum = 123.8996
+// console.log(otherNum.toPrecision(4))
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// -------------------- Maths --------------------
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random())
+console.log(Math.random() * 10 + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const a = 10
+const b = 50
+
+console.log(a + Math.floor(Math.random() * (b - a)))
